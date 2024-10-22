c86fe9afa7852f9f35a5f993673ad8a0e0db3f1f
ChinmayMundane <109454803+ChinmayMundane@users.noreply.github.com>
2024-10-22 12:56:54 +0530

    initiial script for our env

---
+++ gym_multi_car_racing/demo.py
@@ -0,0 +1,39 @@
+import gym
+import gym_multi_car_racing 
+import numpy as np
+
+def my_policy(obs):
+    num_agents = obs.shape[0]  
+    actions = np.zeros((num_agents, 3))  
+
+    for i in range(num_agents):
+        actions[i, 0] = 0  # Steering (always go straight)
+        # actions[i, 0] = np.random.uniform(-0.1, 0.1)
+        actions[i, 1] = 1  # Throttle (always accelerate)
+        actions[i, 2] = 0  # Braking (no braking)
+    return actions
+
+
+env = gym.make("MultiCarRacing-v0", num_agents=2, direction='CCW',
+        use_random_direction=True, backwards_flag=True, h_ratio=0.25,
+        use_ego_color=False)
+
+obs = env.reset()
+done = False
+total_reward = 0
+
+while not done:
+  # The actions have to be of the format (num_agents,3)
+  # The action format for each car is as in the CarRacing-v0 environment.
+  action = my_policy(obs)
+
+  # Similarly, the structure of this is the same as in CarRacing-v0 with an
+  # additional dimension for the different agents, i.e.
+  # obs is of shape (num_agents, 96, 96, 3)
+  # reward is of shape (num_agents,)
+  # done is a bool and info is not used (an empty dict).
+  obs, reward, done, info = env.step(action)
+  total_reward += reward
+  env.render()
+
+print("individual scores:", total_reward)
